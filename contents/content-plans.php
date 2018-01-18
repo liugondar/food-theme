0f0f2e11d656d5196e68aafe137ff8894f50b26b
liugondar <liugondar@gmail.com>
2018-01-18 23:14:18 +0700

    complete section plans

--- contents/content-plans.php
+++ contents/content-plans.php
@@ -1,3 +1,15 @@
+<?php function seperateText($text){
+} ;?>
+<?php 
+    $detail1=get_field('field_5a60c0846f6e0') ;
+    $detail1= preg_split ('/\r\n|\n|\r/', $detail1);
+    $detail2=get_field('field_5a60c0d06f6e4');
+    $detail2= preg_split ('/\r\n|\n|\r/', $detail2);
+
+    $detail3=get_field('field_5a60c1006f6e8');
+    $detail3= preg_split ('/\r\n|\n|\r/', $detail3);
+?>
+
 <section class="section-plans">
     <div class="container">
         <h1>Start eating healthy today</h1>
@@ -7,15 +19,14 @@
                     <div class="plan-box">
                         <div>
                             <h3>Premium</h3>
-                            <p class="plan-price">$399 <span>/ month</span></p>
-                            <p class="plan-price-meal">That's only $13.30 per meal</p>
+                            <p class="plan-price">$<?php echo the_field('field_5a60bfc0b16e8') ;?><span>/ month</span></p>
+                            <p class="plan-price-meal"> <?php echo the_field('field_5a60c0576f6df') ;?></p>
                         </div>
                         <div>
                             <ul>
-                                <li><i class="fa fa-cloud"></i>1 meal every day</li>
-                                <li><i class="fa fa-cloud"></i>Order 24/7</li>
-                                <li><i class="fa fa-cloud"></i>Access to newest creations</li>
-                                <li><i class="fa fa-cloud"></i>Free delivery</li>
+                            <?php foreach($detail1 as $detail): ;?>
+                                <li><i class="fa fa-cloud"></i><?php echo $detail ;?></li>
+                            <?php endforeach ;?>
                             </ul>
                         </div>
                         <div>
@@ -27,19 +38,18 @@
                     <div class="plan-box">
                         <div>
                             <h3>Pro</h3>
-                            <p class="plan-price">$19 <span>/ meal</span></p>
-                            <p class="plan-price-meal">That's only $13.30 per meal</p>
+                            <p class="plan-price">$<?php echo the_field('field_5a60c0ac6f6e2') ;?><span>/ meal</span></p>
+                            <p class="plan-price-meal"><?php echo the_field('field_5a60c0c06f6e3') ;?></p>
                         </div>
                         <div>
                             <ul>
-                                <li><i class="fa fa-cloud"></i>1 meal every day</li>
-                                <li><i class="fa fa-cloud"></i>Order 24/7</li>
-                                <li><i class="fa fa-cloud"></i>Access to newest creations</li>
-                                <li><i class="fa fa-cloud"></i>Free delivery</li>
+                                <?php foreach($detail2 as $detail) :;?>
+                                <li><i class="fa fa-cloud"></i><?php echo $detail ;?></li>
+                                <?php endforeach ;?>
                             </ul>
                         </div>
                         <div>
-                            <a href="#" class="btn btn-ghost">Sign up now</a>
+                            <a href="#" class="btn btn-full">Sign up now</a>
                         </div>
                     </div>
                 </div>
@@ -47,15 +57,14 @@
                     <div class="plan-box">
                         <div>
                             <h3>Starter</h3>
-                            <p class="plan-price">$149 <span>/ month</span></p>
-                            <p class="plan-price-meal">&nbsp;</p>
+                            <p class="plan-price">$<?php echo the_field('field_5a60c0ee6f6e6') ;?><span>/ month</span></p>
+                            <p class="plan-price-meal"><?php echo the_field('field_5a60c0f86f6e7') ;?></p>
                         </div>
                         <div>
                             <ul>
-                                <li><i class="fa fa-cloud"></i>1 meal every day</li>
-                                <li><i class="fa fa-cloud"></i>Order 24/7</li>
-                                <li><i class="fa fa-cloud"></i>Access to newest creations</li>
-                                <li><i class="fa fa-cloud"></i>Free delivery</li>
+                                <?php foreach($detail3 as $detail) :;?>
+                                <li><i class="fa fa-cloud"></i><?php echo $detail ;?></li>
+                                <?php endforeach ;?>
                             </ul>
                         </div>
                         <div>
