37ff5cdf16b8c311a9a84c95e79f908202071c03
liugondar <liugondar@gmail.com>
2018-01-18 22:24:49 +0700

    complete section feedback

--- contents/content-testimonials.php
+++ contents/content-testimonials.php
@@ -1,13 +1,18 @@
+<?php $loop=new WP_Query(array(
+        'post_type'=>'section_feedback',
+        'orderby'=>'post_id',
+        'order'=>'ASC'
+)) ;
+?>
 <section class="section-testimonials ">
     <div class="container">
     <h1>Our customers can't live without us</h1>
     <div class="grid">
-        <blockquote  class="col span-1-of-3">Omnifood is just awesome! I just launched a startup which leaves me with no time for cooking, so Omnifood is a life-saver. Now that I got used to it, I couldn't live without my daily meals!
-<cite><img src="<?php bloginfo('template_directory'); ?>/images/customer-1.jpg" alt="">(Alberto Duncan)</cite></blockquote>
-        <blockquote class="col span-1-of-3">Inexpensive, healthy and great-tasting meals, delivered right to my home. We have lots of food delivery here in Lisbon, but no one comes even close to Omifood. Me and my family are so in love!
-<cite><img src="<?php bloginfo('template_directory'); ?>/images/customer-2.jpg" alt="">(Joana Silva)</cite></blockquote>
-        <blockquote class="col span-1-of-3">I was looking for a quick and easy food delivery service in San Franciso. I tried a lot of them and ended up with Omnifood. Best food delivery service in the Bay Area. Keep up the great work!
-<cite><img src="<?php bloginfo('template_directory'); ?>/images/customer-3.jpg" alt="">(Milton Chapman)</cite></blockquote>
+        <?php while($loop->have_posts()): $loop->the_post() ;?>
+
+        <blockquote  class="col span-1-of-3"><?php the_content() ;?>
+<cite><img src="<?php $temp=get_field('author_img'); echo $temp['url'];  ?> "/>(<?php echo the_field('author') ;?> )</cite></blockquote>
+        <?php endwhile ;?>
 </div>
 
     
